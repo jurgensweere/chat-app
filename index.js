6f835a30c600f314824d984ce13337716fd66cda
Jurgen Sweere <jurgen.sweere@nl.abnamro.com>
2017-10-06 11:05:43 +0200

    Code Styling

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-
-      socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
     });
-  });
+    socket.on('disconnect', function() {
+    });
+});
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
